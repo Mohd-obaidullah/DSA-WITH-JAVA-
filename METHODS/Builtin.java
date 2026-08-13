eb4a36c9fc98119362ede80d89693ec25ff3dfc6
MOHD OBAIDULLAH <mohdobaidullah70@gmail.com>
2026-08-13 21:45:45 +0530

    methods

---
+++ METHODS/Builtin.java
@@ -0,0 +1,10 @@
+public class Builtin {
+    public static void main(String[] args) {
+        // math.wale
+        System.out.println(Math.sqrt(16)); // Output: 4.0
+        System.out.println(Math.abs(-45)); // Output: 45
+        System.out.println(Math.max(10, 20)); // Output: 20
+        System.out.println(Math.min(10, 20)); // Output: 10
+        System.out.println(Math.pow(2, 3)); // Output: 8.0
+    }
+}
